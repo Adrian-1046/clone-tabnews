1802e24656cc27eaff3077c7bbde801adc089204
Adrian Bellezzo Medeiros <98426757+Adrian-1046@users.noreply.github.com>
2024-12-22 18:44:00 +0000

    adicionado página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <p><b>
+        "&Eacute; melhor saber e n&atilde;o precisar, do que precisar e n&atilde;o saber!"</b><br></br> <small>-Desconhecido</small>
+        </p>
+}
+
+export default Home;
