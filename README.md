9f3c766b61db61361578b2dcc5caf9031e10daa6
Adrian Bellezzo Medeiros <98426757+Adrian-1046@users.noreply.github.com>
2024-12-23 23:17:01 +0000

    adicionado e configurado `prettier`

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # clone-tabnews
+
 Implementação do <a href="https://www.tabnews.com.br">tabnews<a> para o curso do <b>filipedeschamps</b> em <a href="https://curso.dev">Curso.dev</a>
 <br><br>
 Acompanhe o resultado <a href="https://clone-tabnews-lyart-kappa.vercel.app/">AQUI</a>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
-    return <p><b>
-        "&Eacute; melhor saber e n&atilde;o precisar, do que precisar e n&atilde;o saber!"</b><br></br> <small>-Desconhecido</small>
-        </p>
+  return (
+    <p>
+      <b>
+        "&Eacute; melhor saber e n&atilde;o precisar, do que precisar e
+        n&atilde;o saber!"
+      </b>
+      <br></br> <small>-Desconhecido</small>
+    </p>
+  );
 }
 
 export default Home;
