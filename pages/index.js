270c62dd96571f2e5271f9576f6664dd84c5b47d
Adrian Medeiros <adrian.blz.medeiros@gmail.com>
2024-12-23 23:59:44 -0300

    alterado mensagem da página

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 function Home() {
   return (
-    <p>
-      <b>
-        "&Eacute; melhor saber e n&atilde;o precisar, do que precisar e
-        n&atilde;o saber!"
-      </b>
-      <br></br> <small>-Desconhecido</small>
-    </p>
+      <h1>
+        Te amo, Lav&iacute;nia!
+      </h1>
+    
   );
 }
 
