d8314da858ad73acfc0da4952954ea04d161dad6
Adrian Medeiros <adrian.blz.medeiros@gmail.com>
2024-12-28 15:02:06 -0300

    add services script

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
   return (
-    <body>
+    <containter>
       <h1>Site em construção!</h1>
       <p>Desenvolvedores envolvidos: Adrian Bellezzo Medeiros, -</p>
       <br></br>
       <small> &copy; Coudray, 2025</small>
-    </body>
+    </containter>
   );
 }
 
