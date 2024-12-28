f603ae36824326f0d2e1a1a12d40fa1ec262d7e4
Adrian Medeiros <adrian.blz.medeiros@gmail.com>
2024-12-28 14:31:47 -0300

    corrigido erro 500 devido a nome das variáveis ambiente estarem incorretas

--- infra/database.js
+++ infra/database.js
@@ -4,9 +4,9 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
   const result = await client.query(queryObject);
