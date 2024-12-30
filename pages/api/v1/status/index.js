f4618291e40a47518536c57953aa4dafa9743913
Adrian Medeiros <adrian.blz.medeiros@gmail.com>
2024-12-29 23:42:15 -0300

    finish endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,24 @@
 import database from "infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS DOIS;");
-  console.log(result);
-  res.status(200).json({
-    status: "200 | OK",
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const smdbVersion = await database.query("SELECT Version();");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: `SELECT COUNT(*)::int AS used_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        smdb_version: smdbVersion.rows[0].version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        used_connections: parseInt(usedConnections.rows[0].used_connections),
+      },
+    },
   });
 }
 export default status;
--- tests/Integration/api/v1/status/get.test.js
+++ tests/Integration/api/v1/status/get.test.js
@@ -2,3 +2,33 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://127.0.0.1:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+test("/api/v1/status/ UPDATED_AT has to be valid new date", async () => {
+  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("/api/v1/status/ SMDBD_VERSION has to be valid version", async () => {
+  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.smdb_version).toBeDefined();
+  expect(responseBody.dependencies.database.smdb_version).not.toBeNull();
+});
+
+test("/api/v1/status/ SMDB_MAX_CONNECTIONS has to be valid and equal to 100 ", async () => {
+  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+});
+
+test("/api/v1/status/ SMDB_USED_CONNECTIONS has to be valid and equal to 1 in development ", async () => {
+  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.used_connections).toEqual(1);
+});
