5cc0d370d82582942f18885c2bdbf4573395a83d
Adrian Medeiros <adrian.blz.medeiros@gmail.com>
2025-01-03 21:40:48 -0300

    test(tests): rename all descriptions to use new pattern in tests

--- tests/Integration/api/v1/migrations/head.test.js
+++ tests/Integration/api/v1/migrations/head.test.js
@@ -1,7 +1,28 @@
-test("HEAD to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/migrations", {
-    method: "HEAD",
+describe("HEAD/DELETE/PUT /api/v1/migrations", () => {
+  describe("Aonymous user", () => {
+    test("Methods different from GET and POST should not work", async () => {
+      const responseToHead = await fetch(
+        "http://127.0.0.1:3000/api/v1/migrations",
+        {
+          method: "HEAD",
+        },
+      );
+      const responseToDelete = await fetch(
+        "http://127.0.0.1:3000/api/v1/migrations",
+        {
+          method: "DELETE",
+        },
+      );
+      const responseToPut = await fetch(
+        "http://127.0.0.1:3000/api/v1/migrations",
+        {
+          method: "PUT",
+        },
+      );
+
+      expect(responseToHead.status).toBe(405);
+      expect(responseToDelete.status).toBe(405);
+      expect(responseToPut.status).toBe(405);
+    });
   });
-  //const response1Body = await response.json();
-  expect(response.status).toBe(405);
 });
